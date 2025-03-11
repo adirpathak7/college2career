25af7a79b71e5704498feefaf7cdb3b3a4556317
adirpathak7 <adityarpathak7@gmail.com>
2025-03-11 16:40:50 +0530

    signUp page validation

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 
 export default function SignUp() {
@@ -12,18 +12,71 @@ export default function SignUp() {
         role: ''
     })
 
+    const navigate = useNavigate()
+
     const handelInputChange = (e) => {
         const { name, value } = e.target
-        console.log(value);
+        // console.log(value);
         setInputData((preValue) => ({
             ...preValue,
             [name]: value
         }))
+        clearError("username")
+        clearError("email")
+        clearError("password")
+        clearError("confirmPassword")
+        clearError("role")
+    }
+
+    const clearError = (field) => {
+        document.getElementById(field + "Error").innerHTML = ''
     }
 
     const handelSubmit = (e) => {
         e.preventDefault()
-        alert(1)
+        let username = document.getElementById("username").value
+        let email = document.getElementById("email").value
+        let password = document.getElementById("password").value
+        let confirmPassword = document.getElementById("confirmPassword").value
+        // let role = document.getElementById("role").value
+
+        if (!username) {
+            document.getElementById("usernameError").innerHTML = "Please enter the Username!"
+            document.getElementById("username").focus()
+            return false
+        } else if (!email) {
+            document.getElementById("emailError").innerHTML = "Please enter the Email!"
+            document.getElementById("email").focus()
+            return false
+        } else if (!password) {
+            document.getElementById("passwordError").innerHTML = "Please enter the Password!"
+            document.getElementById("password").focus()
+            return false
+        } else if (!confirmPassword) {
+            document.getElementById("confirmPasswordError").innerHTML = "Please enter the Confirm Password!"
+            document.getElementById("confirmPassword").focus()
+            return false
+        } else if (confirmPassword !== password) {
+            document.getElementById("confirmPasswordError").innerHTML = "Confirm Password and Password should be match!"
+            document.getElementById("confirmPassword").focus()
+            return false
+        } else {
+            alert('Registration successfully')
+            setInputData({
+                username: '',
+                email: '',
+                password: '',
+                confirmPassword: '',
+                role: ''
+            })
+            navigate('/signIn')
+            return true
+        }
+        // else if (role === "default") {
+        //     document.getElementById("roleError").innerHTML = "Please select the role!"
+        //     document.getElementById("role").focus()
+        //     return false
+        // }
     }
 
     return (
@@ -35,25 +88,31 @@ export default function SignUp() {
                         <div className="mb-4">
                             <label className="block mb-1">Username</label>
                             <input value={inputData.username} onChange={handelInputChange} type="text" id='username' name='username' className="w-full p-2 border border-gray-700 bg-gray-800 rounded focus:outline-none focus:border-white" />
+                            <span id='usernameError' className='text-red-600'></span>
                         </div>
                         <div className="mb-4">
                             <label className="block mb-1">Email</label>
-                            <input value={inputData.email} onChange={handelInputChange} type="email" id='email' name='email' className="w-full p-2 border border-gray-700 bg-gray-800 rounded focus:outline-none focus:border-white" />
+                            <input value={inputData.email} onChange={handelInputChange} type="text" id='email' name='email' className="w-full p-2 border border-gray-700 bg-gray-800 rounded focus:outline-none focus:border-white" />
+                            <span id='emailError' className='text-red-600'></span>
                         </div>
                         <div className="mb-4">
                             <label className="block mb-1">Password</label>
                             <input value={inputData.password} onChange={handelInputChange} type="password" id='password' name='password' className="w-full p-2 border border-gray-700 bg-gray-800 rounded focus:outline-none focus:border-white" />
+                            <span id='passwordError' className='text-red-600'></span>
                         </div>
                         <div className="mb-4">
                             <label className="block mb-1">Confirm Password</label>
-                            <input value={inputData.confirmPassword} onChange={handelInputChange} type="password" id='confirmPassword' className="w-full p-2 border border-gray-700 bg-gray-800 rounded focus:outline-none focus:border-white" />
+                            <input value={inputData.confirmPassword} onChange={handelInputChange} type="password" id='confirmPassword' name='confirmPassword' className="w-full p-2 border border-gray-700 bg-gray-800 rounded focus:outline-none focus:border-white" />
+                            <span id='confirmPasswordError' className='text-red-600'></span>
                         </div>
                         <div className="mb-4">
                             <label className="block mb-1">Join As</label>
-                            <select value={inputData.role} onChange={handelInputChange} className="w-full p-2 border border-gray-700 bg-gray-800 rounded focus:outline-none focus:border-white" name="role" id="role">
+                            <select value={inputData.role} onChange={handelInputChange} name="role" id="role" className="w-full p-2 border border-gray-700 bg-gray-800 rounded focus:outline-none focus:border-white">
+                                {/* <option value="default">Join As</option> */}
                                 <option value="company">Company</option>
                                 <option value="student">Student</option>
                             </select>
+                            <span id='roleError' className='text-red-600'></span>
                         </div>
 
                         <button type="submit" className="w-full bg-white text-black py-2 rounded-lg hover:bg-gray-300 font-bold transition duration-300">
